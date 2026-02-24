e04827ae4f17857c3d8bbf5b84c94e8df4111eca
vincent.nguyen <vincent.nguyen@cognizant.com>
2026-02-24 03:24:49 -0800

    added new test case to smoke test suite

--- tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
+++ tests/neuro_san/zzz_hocons/test_smoke_test_hocons.py
@@ -64,6 +64,7 @@ class TestSmokeTestHocons(TestCase):
         # Ideally more basic functionality will come first.
         # Barring that, try to stick to alphabetical order.
         "music_nerd_pro/combination_responses_with_history_mcp.hocon",
+        "copy_cat/create_and_use_temp_network_http.hocon",
         # Issue #734:
         # "music_nerd_pro_sly/combination_responses_with_history_mcp.hocon",
 
