c77391582c125e3dbfaa0c54bc5c0724978bf618
Dan Fink <Daniel.Fink@cognizant.com>
2026-03-03 16:34:30 -0800

    Make LlmInfoRestorer an AbstractAsyncConfigRestorer

--- neuro_san/internals/run_context/langchain/llms/llm_info_restorer.py
+++ neuro_san/internals/run_context/langchain/llms/llm_info_restorer.py
@@ -15,55 +15,34 @@
 #
 # END COPYRIGHT
 
-from typing import Any
-from typing import Dict
-
-import json
-
-from pyparsing.exceptions import ParseException
-from pyparsing.exceptions import ParseSyntaxException
-
-from leaf_common.persistence.easy.easy_hocon_persistence import EasyHoconPersistence
-from leaf_common.persistence.interface.restorer import Restorer
-
 from neuro_san import TOP_LEVEL_DIR
+from neuro_san.internals.persistence.abstract_async_config_restorer import AbstractAsyncConfigRestorer
 
 
-class LlmInfoRestorer(Restorer):
+class LlmInfoRestorer(AbstractAsyncConfigRestorer):
     """
-    Implementation of the Restorer interface to read in an LlmInfo dictionary
+    Implementation of the AbstractAsyncConfigRestorer interface to read in an LlmInfo dictionary
     instance given a hocon file name.
+    The restore() and async_restore() methods both return dictionary instances.
     """
 
-    def restore(self, file_reference: str = None):
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__(file_purpose="llm_info")
+
+    def get_file_path(self, file_reference: str = None) -> str:
         """
         :param file_reference: The file reference to use when restoring.
                 Default is None, implying the file reference is up to the
                 implementation.
         :return: an object from some persisted store
         """
-        config: Dict[str, Any] = None
-
         use_file: str = file_reference
 
         if file_reference is None or len(file_reference) == 0:
             # Read from the default
             use_file = TOP_LEVEL_DIR.get_file_in_basis("internals/run_context/langchain/llms/default_llm_info.hocon")
 
-        try:
-            if use_file.endswith(".json"):
-                config = json.load(use_file)
-            elif use_file.endswith(".hocon"):
-                hocon = EasyHoconPersistence(full_ref=use_file, must_exist=True)
-                config = hocon.restore()
-            else:
-                raise ValueError(f"file_reference {use_file} must be a .json or .hocon file")
-        except (ParseException, ParseSyntaxException, json.decoder.JSONDecodeError) as exception:
-            message = f"""
-There was an error parsing the llm_info file "{use_file}".
-See the accompanying ParseException (above) for clues as to what might be
-syntactically incorrect in that file.
-"""
-            raise ParseException(message) from exception
-
-        return config
+        return use_file
