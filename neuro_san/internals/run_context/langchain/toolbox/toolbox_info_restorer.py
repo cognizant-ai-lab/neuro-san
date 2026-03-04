0e448388ca72cedda93f1f63c4cde562539cc1d5
Dan Fink <Daniel.Fink@cognizant.com>
2026-03-04 10:04:41 -0800

    Fix typo

--- neuro_san/internals/run_context/langchain/toolbox/toolbox_info_restorer.py
+++ neuro_san/internals/run_context/langchain/toolbox/toolbox_info_restorer.py
@@ -37,7 +37,7 @@ class ToolboxInfoRestorer(AbstractAsyncConfigRestorer):
         :param file_reference: The file reference to use when restoring.
                 Default is None, implying the file reference is up to the
                 implementation.
-        :return: a string represting the file path to use
+        :return: a string representing the file path to use
         """
         use_file: str = file_reference
 
