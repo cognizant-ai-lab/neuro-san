1175cd0dbda26c5f9ea66b390d773be0aa23fe59
Dan Fink <Daniel.Fink@cognizant.com>
2026-04-03 10:56:42 -0700

    Make the ServiceAgentReservationist a LingeringResource

--- neuro_san/internals/chat/data_driven_chat_session.py
+++ neuro_san/internals/chat/data_driven_chat_session.py
@@ -407,8 +407,8 @@ class DataDrivenChatSession(RunTarget, LingeringResource):
         # Now that we are done, tell the Reservationist that we used for this request
         # that there will be no more Reservations to corral.
         reservationist: Reservationist = self.invocation_context.get_reservationist()
-        if reservationist is not None:
-            await reservationist.close()
+        if reservationist is not None and isinstance(reservationist, LingeringResource):
+            await reservationist.close_of_work()
 
         # Close any objects on sly data that can be closed.
         await self.close_sly_data()
