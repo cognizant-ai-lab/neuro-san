ffb91bae65059a97e4e585c6fd39fcb74c8f2a43
Dan Fink <Daniel.Fink@cognizant.com>
2026-04-14 08:28:01 -0700

    Fix MessageFilter missing last AgentFramework message in error situations

--- neuro_san/internals/chat/data_driven_chat_session.py
+++ neuro_san/internals/chat/data_driven_chat_session.py
@@ -168,8 +168,9 @@ class DataDrivenChatSession(RunTarget, LingeringResource):
             # This can happen if the user is trying to send a new message
             # while it is still working on a previous message that has not
             # yet returned.
+            empty: Dict[str, Any] = {}
             raw_messages: List[BaseMessage] = [
-                AgentFrameworkMessage(content="Patience, please. I'm working on it.")
+                AgentFrameworkMessage(content="Patience, please. I'm working on it.", chat_context=empty)
             ]
 
             logger: Logger = getLogger(self.__class__.__name__)
@@ -256,8 +257,8 @@ class DataDrivenChatSession(RunTarget, LingeringResource):
 
         # If we are invoked as an event, tell the caller that it's OK to disconnect early.
         # By definition, they are not expecting a custom response.
+        empty: Dict[str, Any] = {}
         if self.invocation_context.get_effective_invocation() == "event":
-            empty: Dict[str, Any] = {}
             event_acknowledge = AgentFrameworkMessage(content="Event acknowledged", chat_context=empty)
             await self.finalize_request(event_acknowledge)
 
@@ -274,7 +275,7 @@ class DataDrivenChatSession(RunTarget, LingeringResource):
             except ValueError as exc:
                 # This can happen if we have problems with LLM clients API keys:
                 # Construct a message to send back to the client with the error information.
-                message = AgentFrameworkMessage(content=str(exc))
+                message = AgentFrameworkMessage(content=str(exc), chat_context=empty)
                 await self.finalize_request(message)
                 return message
 
