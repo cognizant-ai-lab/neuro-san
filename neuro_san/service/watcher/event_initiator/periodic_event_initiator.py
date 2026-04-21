db43587898b1b1e76a1800cbbe3af8399a223136
Dan Fink <Daniel.Fink@cognizant.com>
2026-04-21 09:16:01 -0700

    Add some comments

--- neuro_san/service/watcher/event_initiator/periodic_event_initiator.py
+++ neuro_san/service/watcher/event_initiator/periodic_event_initiator.py
@@ -20,7 +20,6 @@ from typing import AsyncGenerator
 from typing import Awaitable
 from typing import Dict
 from typing import List
-from typing import Tuple
 
 from asyncio import Task
 from datetime import datetime
@@ -57,14 +56,18 @@ class PeriodicEventInitiator(WatcherThread):
         """
         Main loop
         """
-        periodic_configs: Dict[str, Dict[str, Any]] = self.server_context.get_periodic_configs()
-
-        now: datetime = datetime.now()
-        iterators: Dict[str, Dict[Dict[str, Any], CronIter]] = self.set_up_iterators(now, periodic_configs)
-
+        # Start the executor so our async event initiators have someplace to run
         self.executor.start()
 
-        next_firing: Dict[Tuple[str, Dict[str, Any]], datetime] = {}
+        # Get the periodic configs from the ServerContext
+        periodic_configs: Dict[str, Dict[str, Any]] = self.server_context.get_periodic_configs()
+
+        # Set up the periodic agent interactions in a list
+        now: datetime = datetime.now()
+        agent_interaction_list: List[Dict[str, Any]] = self.set_up_agent_interactions(now, periodic_configs)
+
+        # Keep track of the next firing times for each index in the agent_interaction_list
+        next_firing: Dict[int, datetime] = {}
 
         while self.should_keep_running():
 
@@ -72,21 +75,11 @@ class PeriodicEventInitiator(WatcherThread):
             start: datetime = datetime.now()
 
             # Update the next firing dictionary for all the periodic agents
-            next_firing = self.update_next_firing(start, iterators, next_firing)
+            next_firing = self.update_next_firing(start, agent_interaction_list, next_firing)
 
-            fire_these_now: Dict[str, Dict[str, Any]] = {}
-
-            # Loop through all the next firing times to see which ones are in the past.
-            # Those we want to fire now.
-            tuple_key: Tuple[str, Dict[str, Any]] = None
-            next_firing_time: datetime = None
-            for tuple_key, next_firing_time in next_firing.items():
-
-                # If the next firing time is in the past, record that we need to initiate the agent
-                if next_firing_time < start:
-                    agent_network: str = tuple_key[0]
-                    periodic_config: Dict[str, Any] = tuple_key[1]
-                    fire_these_now[agent_network] = periodic_config
+            # Figure out which periodic agents need to fire now
+            fire_these_now: Dict[str, Dict[str, Any]] = self.figure_which_to_fire(start, agent_interaction_list,
+                                                                                  next_firing)
 
             # Fire off the periodic agents we need to for this iteration
             self.fire_all_these_now(fire_these_now)
@@ -96,119 +89,156 @@ class PeriodicEventInitiator(WatcherThread):
 
         self.executor.shutdown()
 
-    def set_up_iterators(
+    def set_up_agent_interactions(
                 self,
                 start_time: datetime,
                 periodic_configs: Dict[str, Dict[str, Any]]
-            ) -> Dict[str, Dict[Dict[str, Any], CronIter]]:
+            ) -> List[Dict[str, Any]]:
         """
-        Sets up the Cron iterators
+        Sets up a list of all periodic agent interactions.
+        We need this as a list because we cannot use a non-hashable periodic config
+        as a key for a dictionary or tuple, but the index into this list is hashable.
 
-        :param start_time: the start time basis for the CronIters
-        :return: a mapping of agent_network to another mapping of interaction dictionary -> CronIter
+        :param start_time: the start time
+        :param periodic_configs: the dictionary of agent_network -> periodic configs
+        :return: a list of periodic agent interactions
         """
         # What we will return
-        iterators: Dict[str, Dict[Dict[str, Any], CronIter]] = {}
+        agent_interaction_list: List[Dict[str, Any]] = []
+        empty_list: List[Dict[str, Any]] = []
 
-        # Loop through the periodic configs
-        for agent_network, config in periodic_configs.items():
+        # Loop through the periodic configs, adding a new dictionary for each single agent interaction
+        for agent_network, periodic_config in periodic_configs.items():
 
-            # Find all the interactions in the config
-            empty_list: List[Dict[str, Any]] = []
-            interactions: List[Dict[str, Any]] = config.get("interactions", empty_list)
-            interaction_to_iterator: Dict[Dict[str, Any], CronIter] = {}
+            # Loop through the interactions
+            interactions: List[Dict[str, Any]] = periodic_config.get("interactions", empty_list)
             for interaction in interactions:
 
                 # Only look at enabled interactions
-                if interaction.get("enable", True):
+                if not interaction.get("enable", True):
+                    continue
 
-                    # Get the string representing the cron schedule
-                    cron_schedule: str = interaction.get("cron_schedule",
-                                                         PeriodicManifestDictConfigFilter.DEFAULT_CRON_SCHEDULE)
-                    # Put an iterator in the inner mapping
-                    iterator = CronIter(cron_schedule, start_time)
-                    interaction_to_iterator[interaction] = iterator
+                # Get the string representing the cron schedule
+                cron_schedule: str = interaction.get("cron_schedule",
+                                                     PeriodicManifestDictConfigFilter.DEFAULT_CRON_SCHEDULE)
+                # Put an iterator in the inner mapping
+                iterator = CronIter(cron_schedule, start_time)
 
-            # Put the inner mapping in the outer mapping
-            iterators[agent_network] = interaction_to_iterator
+                # Create an agent interaction dictionary for the list
+                agent_interaction: Dict[str, Any] = {
+                    "agent_interaction": agent_network,
+                    "interaction": interaction,
+                    "cron_iter": iterator
+                }
+                agent_interaction_list.append(agent_interaction)
 
-        return iterators
+        return agent_interaction_list
 
     def update_next_firing(
                 self,
                 now: datetime,
-                iterators: Dict[str, Dict[Dict[str, Any], CronIter]],
-                next_firing: Dict[Tuple[str, Dict[str, Any]], datetime]
-            ) -> Dict[Tuple[str, Dict[str, Any]], datetime]:
+                agent_interaction_list: List[Dict[str, Any]],
+                next_firing: Dict[int, datetime]
+            ) -> Dict[int, datetime]:
         """
         Updates the next firing times
 
         :param now: The current time
-        :param iterators: A mapping of agent_network to another mapping of interaction dictionary -> CronIter
-        :param next_firing: A mapping of agent_network/interaction dictionary to datetime of next firing
+        :param agent_interaction_list: A list of single agent interactions
+        :param next_firing: A mapping of agent_interaction_list index -> datetime of next firing
         :return: A new next firing mapping
         """
         # What we will return
-        new_next_firing: Dict[Tuple[str, Dict[str, Any]], datetime] = {}
-
-        agent_network: str = None
-        interaction_to_iterator: Dict[Dict[str, Any], CronIter] = None
+        new_next_firing: Dict[int, datetime] = {}
 
         # Loop through the outer mapping whose key is the agent network name
-        for agent_network, interaction_to_iterator in iterators.items():
+        for index, agent_interaction in enumerate(agent_interaction_list):
 
-            interaction: Dict[str, Any] = None
-            cron_iter: CronIter = None
+            # See if we already have a next firing
+            next_firing_time: datetime = next_firing.get(index)
+            if next_firing_time is not None:
 
-            # Loop through the inner mapping of interaction dict to CronIter
-            for interaction, cron_iter in interaction_to_iterator.items():
+                # If the next firing time is in the future, don't disturb it.
+                if next_firing_time > now:
+                    new_next_firing[index] = next_firing_time
+                    continue
 
-                # See if we already have a next firing
-                tuple_key: Tuple[str, Dict[str, Any]] = (agent_network, interaction)
-                next_firing_time: datetime = next_firing.get(tuple_key)
-                if next_firing_time is not None:
+            # Get a new next firing for this agent_interaction
+            cron_iter: CronIter = agent_interaction.get("cron_iter")
 
-                    # If the next firing time is in the future, don't disturb it.
-                    if next_firing_time > now:
-                        new_next_firing[tuple_key] = next_firing_time
-                        continue
+            # Need to pass the type of what we want back from the iterator
+            next_firing_time = cron_iter.get_next(datetime)
+            new_next_firing[index] = next_firing_time
 
-                # Get a new next firing for this agent_network
-                next_firing_time = cron_iter.get_next(datetime)
-                new_next_firing[tuple_key] = next_firing_time
-
-                # Slow event processing might require skipping/compressing
-                # some iterator times that are in the past, but not there yet.
+            # Slow event processing might require skipping/compressing
+            # some iterator times that are in the past, but not there yet.
 
         return new_next_firing
 
+    def figure_which_to_fire(
+                self,
+                now: datetime,
+                agent_interaction_list: List[Dict[str, Any]],
+                next_firing: Dict[int, datetime]
+            ) -> Dict[str, Dict[str, Any]]:
+        """
+        Figures out which periodic agent interactions need to fire now.
+
+        :param now: The datetime corresponding to the current moment
+        :param agent_interaction_list: A list of single agent interactions
+        :param next_firing: A mapping of agent_interaction_list index -> datetime of next firing
+        :return: A mapping of agent_network -> periodic config
+        """
+        # What we will return
+        fire_these_now: Dict[str, Dict[str, Any]] = {}
+
+        # Loop through all the next firing times to see which ones are in the past.
+        # Those we want to fire now.
+        agent_interaction_index: int = None
+        next_firing_time: datetime = None
+        for agent_interaction_index, next_firing_time in next_firing.items():
+
+            # If the next firing time is in the past, record that we need to initiate the agent
+            if next_firing_time < now:
+                # Look up the agent interaction given the index
+                agent_interaction: Dict[str, Any] = agent_interaction_list[agent_interaction_index]
+
+                # Update the dictionary with agent_network -> interaction
+                agent_network: str = agent_interaction.get("agent_interaction")
+                interaction: Dict[str, Any] = agent_interaction.get("interaction")
+                fire_these_now[agent_network] = interaction
+
+        return fire_these_now
+
     def fire_all_these_now(self, fire_these_now: Dict[str, Dict[str, Any]]):
         """
-        Pokes the given agent_network keys with input described by
-        the periodic_config values.
+        Pokes the given agent_network keys with input described by the interaction values.
 
-        :param agent_network: The agent_network to poke
-        :param periodic_config: The periodic config
+        :param fire_these_now: A mapping of agent_network -> interaction config
         """
         if not fire_these_now:
+            # Nothing to do
             return
 
-        for agent_network, periodic_config in fire_these_now.items():
-            coroutine: Awaitable = self.initiate_event(agent_network, periodic_config)
+        for agent_network, interaction in fire_these_now.items():
+            # Create a task for each agent interaction
+            coroutine: Awaitable = self.initiate_event(agent_network, interaction)
             _: Task = self.executor.create_task(coroutine, self.__class__.__name__)
             # Note that we don't really care when the task completes, just as long as it runs.
 
-    async def initiate_event(self, agent_network: str, periodic_config: Dict[str, Any]):
+    async def initiate_event(self, agent_network: str, interaction: Dict[str, Any]):
         """
-        Pokes the given agent_network with the periodic_config
+        Pokes the given agent_network with the interaction
 
         :param agent_network: The agent_network to poke
-        :param periodic_config: The periodic config to use for the event
+        :param interaction: The interaction config to use for the event
         """
+        # Get the agent authorizer that is going to tell us whether we are allowed to initiate the event
         agent_authorizer: AgentAuthorizer = self.server_context.get_agent_authorizer()
 
+        # Get any request metadata from the interaction
         empty_dict: Dict[str, Any] = {}
-        metadata: Dict[str, Any] = periodic_config.get("metadata", empty_dict)
+        metadata: Dict[str, Any] = interaction.get("metadata", empty_dict)
 
         authorized: bool = False
         service_provider: AsyncAgentServiceProvider = None
@@ -217,20 +247,26 @@ class PeriodicEventInitiator(WatcherThread):
             self.logger.warning("Not authorized to initiate periodic event for agent_network %s", agent_network)
             return
 
-        agent_service: AsyncAgentService = service_provider.get_service()
+        # We are allowed to initiate the event and we have the service_provider with which to do that.
 
+        # Formulate the request to send to the agent
         request_dict: Dict[str, Any] = {
             "user_message": {
                 "type": ChatMessageType.HUMAN.name,
-                "text": periodic_config.get("text", ""),
-                "sly_data": periodic_config.get("sly_data", empty_dict),
+                "text": interaction.get("text", ""),
+                "sly_data": interaction.get("sly_data", empty_dict),
                 "chat_filter": {
-                    "chat_filter_type": "MINIMAL"   # Always minimal for event invocation.
+                    # Always minimal for event invocation.
+                    "chat_filter_type": "MINIMAL"
                 }
             }
         }
 
+        # Perform a streaming_chat() on the agent with the formulated request
+        agent_service: AsyncAgentService = service_provider.get_service()
         results: AsyncGenerator[Dict[str, Any], None] = agent_service.streaming_chat(request_dict, metadata)
+
+        # Wait for the results
         async for result_dict in results:
             # We don't care about the results.
             # We just want to poke the agent and wait for it to be done.
