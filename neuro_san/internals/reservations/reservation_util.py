848dbd7d5d70efdb58dcfa420304e1258528605e
Andrei Denissov <andrei.denissov@cognizant.com>
2026-05-05 16:33:12 -0700

    Change error message.

--- neuro_san/internals/reservations/reservation_util.py
+++ neuro_san/internals/reservations/reservation_util.py
@@ -56,8 +56,8 @@ class ReservationUtil:
         reservationist: Reservationist = args.get("reservationist")
         if reservationist is None:
             error = """
-Reservationist is None.  Try this for your server:
-    export AGENT_TEMPORARY_NETWORK_UPDATE_PERIOD_SECONDS=5
+Reservationist is None.  Make sure that temporary networks reservations
+    are allowed in agent network definition.
 """
             return (reservation, error)
 
