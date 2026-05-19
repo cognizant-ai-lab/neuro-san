3e48de1b05c94efe2b78009041ad2eb760237129
Dan Fink <Daniel.Fink@cognizant.com>
2026-05-19 16:51:26 -0700

    Progress towards a single trace. parent traces still off

--- neuro_san/internals/run_context/langchain/tracing/langfuse_tracing_context.py
+++ neuro_san/internals/run_context/langchain/tracing/langfuse_tracing_context.py
@@ -54,6 +54,7 @@ class LangfuseTracingContext(LangChainTracingContext):
 
         self.parent_context: LangfuseTracingContext = parent_context
         self.callback_handler: BaseCallbackHandler = None
+        self.main_span: Any = None
 
         # See if we can get a langfuse handler instance.
         handler_type = ResolverUtil.create_type("langfuse.langchain.CallbackHandler", raise_if_not_found=False)
@@ -67,8 +68,6 @@ If you really wanted to use langfuse for observability, you can install it with
 If you didn't mean to use langfuse for observability, you can do this:
     export LANGFUSE_ENABLED=false
 """)
-
-        self.maybe_create_handler()
 
     def clone(self) -> TracingContext:
         """
@@ -86,9 +85,6 @@ If you didn't mean to use langfuse for observability, you can do this:
         :param inputs: The inputs to the chain
         :param runnable_config: The config for the runnable
         """
-        # Augment the callbacks with the handler.
-        runnable_config = self.augment_config(runnable_config)
-
         # Get the user_id for the trace
         empty: Dict[str, Any] = {}
         request_metadata: Dict[str, Any] = runnable_config.get("metadata", empty)
@@ -102,10 +98,6 @@ If you didn't mean to use langfuse for observability, you can do this:
         hostname: str = gethostname()
         session_id: str = f"{session_id}@{hostname}"
 
-        # We have a handler, therefore we have langfuse installed.
-        # No need to ResolverUtil absolutely everything, but we still need to locally import
-        # for the rest of the system to behave without langfuse installed.
-
         # pylint: disable=import-outside-toplevel
         from langfuse import propagate_attributes
 
@@ -115,13 +107,49 @@ If you didn't mean to use langfuse for observability, you can do this:
         # It works. :shrug:
         # According to langfuse docs, this should be safe for use in async code.
         # pylint: disable=not-context-manager
-        with propagate_attributes(user_id=user_id, session_id=session_id):
-            await super().ainvoke(chain, inputs, runnable_config)
+        print(f"using main_span as context manager: {self.main_span}")
+        with self.main_span:
+            print("with main_span")
+            with propagate_attributes(user_id=user_id, session_id=session_id):
+                print("with propagate_attributes")
+                await super().ainvoke(chain, inputs, runnable_config)
 
-    def maybe_create_handler(self):
+    def create_main_span(self, runnable_config: Dict[str, Any]):
+
+        if self.main_span is not None:
+            return
+
+        run_name: str = runnable_config.get("run_name")
+
+        # We have a handler, therefore we have langfuse installed.
+        # No need to ResolverUtil absolutely everything, but we still need to locally import
+        # for the rest of the system to behave without langfuse installed.
+
+        # pylint: disable=import-outside-toplevel
+        from langfuse import get_client
+
+        print(f"about to get main_span with run_name: {run_name}")
+        if self.parent_context is not None:
+            # Get the langfuse client
+            print("Parent context is not none")
+            parent_span: Any = self.parent_context.main_span
+            print(f"Using parent context main_span as basis {parent_span} type {parent_span.__class__.__name__}")
+            # self.main_span = parent_span.start_as_current_observation(run_name, as_type="span")
+            langfuse_client: Any = get_client()
+            self.main_span = langfuse_client.start_as_current_observation(as_type="span", name=run_name)
+        else:
+            # Get the langfuse client
+            print("parent context is none")
+            langfuse_client: Any = get_client()
+            self.main_span = langfuse_client.start_as_current_observation(as_type="agent", name=run_name)
+        print(f"main_span: {self.main_span}")
+
+    def maybe_create_handler(self, runnable_config: Dict[str, Any]):
         """
         If the tracing config doesn't have a handler, create it.
         """
+        self.create_main_span(runnable_config)
+
         if self.callback_handler is None and self.parent_context is not None:
             self.callback_handler = self.parent_context.callback_handler
 
@@ -139,6 +167,7 @@ If you didn't mean to use langfuse for observability, you can do this:
                 self.callback_handler = None
                 return
 
+            print(f"about to create langfuse handler with main_span: {self.main_span}")
             self.callback_handler = callback_handler_type()
 
         print(f"using langfuse handler: {self.callback_handler}")
@@ -149,13 +178,10 @@ If you didn't mean to use langfuse for observability, you can do this:
         :param runnable_config: The config for the runnable
         :return: The augmented config
         """
+        self.maybe_create_handler(runnable_config)
         if self.callback_handler is None:
             # Nothing we can do. Skip.
             return runnable_config
-
-        # trace_id: str = self.callback_handler.get_trace_id()
-        # trace_id: str = "unknown"
-        # print(f"    with trace_id: {trace_id}")
 
         # Set the callbacks per the langfuse docs
         callbacks: List[BaseCallbackHandler] = runnable_config.get("callbacks", [])
@@ -164,14 +190,5 @@ If you didn't mean to use langfuse for observability, you can do this:
         runnable_config["callbacks"] = callbacks
 
         runnable_config["neuro_san_tracing_context"] = self
-
-        # Get the run_name, which is the full name of the runnable with origin information
-        # run_name: str = runnable_config.get("run_name")
-        # if run_name:
-        #     empty: Dict[str, Any] = {}
-        #     metadata: Dict[str, Any] = runnable_config.get("metadata", empty)
-        #     metadata["langfuse_trace_name"] = run_name
-        #     runnable_config["metadata"] = metadata
-        #     print(f"metadata: {metadata}")
 
         return runnable_config
