7840d3c6ae01168595f1bf3247fca0289927194f
Dan Fink <Daniel.Fink@cognizant.com>
2026-05-21 11:13:04 -0700

    Do spans for child

--- neuro_san/internals/run_context/langchain/tracing/langfuse_tracing_context.py
+++ neuro_san/internals/run_context/langchain/tracing/langfuse_tracing_context.py
@@ -146,10 +146,6 @@ If you didn't mean to use langfuse for observability, you can do this:
             # Langfuse is not enabled
             return
 
-        if self.parent_context is not None:
-            # This is a child run
-            return
-
         # According to langfuse docs, this should be safe for use in async code.
         run_name: str = runnable_config.get("run_name")
         self.main_span = self.langfuse_client.start_as_current_observation(as_type="agent", name=run_name)
