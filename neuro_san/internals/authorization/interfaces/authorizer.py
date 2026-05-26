d448d6d0c5441363a6a0de2ecb01b7db8f39222c
Dan Fink <Daniel.Fink@cognizant.com>
2026-05-26 09:09:41 -0700

    Add comments to the Authorizer interface to stem off confusion

--- neuro_san/internals/authorization/interfaces/authorizer.py
+++ neuro_san/internals/authorization/interfaces/authorizer.py
@@ -25,7 +25,13 @@ from typing import List
 class Authorizer:
     """
     An interface for authorization.
-    This is based on what we need from what packages like OpenFGA or Oso provide.
+
+    Note that _authorization_ - the ability to determine permission to access a resource (in
+    Neuro SAN's case an agent network) - is not to be confused with _authentication_ -
+    the ability to determine a user's identity.  Normally, authentication is done by
+    a system outside the scope of a Neuro SAN server, like a load-balancer for a cluster.
+
+    The methods here are based on what we need from what packages like OpenFGA or Oso provide.
     """
 
     async def __aenter__(self) -> Authorizer:
