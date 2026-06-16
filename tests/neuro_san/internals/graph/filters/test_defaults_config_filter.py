bb9135a73d23e82ea2e0fbcde343e982ba6ddce9
Dan Fink <Daniel.Fink@cognizant.com>
2026-06-15 17:55:50 -0700

    More tests

--- tests/neuro_san/internals/graph/filters/test_defaults_config_filter.py
+++ tests/neuro_san/internals/graph/filters/test_defaults_config_filter.py
@@ -16,6 +16,7 @@
 # END COPYRIGHT
 from typing import Any
 from typing import Dict
+from typing import List
 
 import json
 
@@ -85,7 +86,7 @@ class TestDefaultsConfigFilter(TestCase):
                                 "description": "The user's OpenAI API key"
                             }
                         }
-                    }        
+                    }
                 },
                 "required": ["llm_config"]
             },
@@ -113,3 +114,110 @@ class TestDefaultsConfigFilter(TestCase):
         self.assertIsNotNone(value)
         self.assertIsInstance(value, list)
         self.assertEqual(len(value), 1)
+
+    def test_local_sly_data_schema_only(self):
+        """
+        Tests when sly_data_schema is in the local defs only
+        """
+        my_filter: ConfigFilter = self.get_filter()
+
+        dict_in: Dict[str, Any] = {
+            "tools": [
+                {
+                    "name": "foo",
+                    "function": {
+                        "description": "blah blah",
+                        "sly_data_schema": {
+                            "type": "object",
+                            "properties": {
+                                "custom_value": {
+                                    "type": "string",
+                                    "description": "A custom string value"
+                                }
+                            },
+                            "required": ["custom_value"]
+                        }
+                    }
+                }
+            ]
+        }
+
+        dict_out: Dict[str, Any] = my_filter.filter_config(dict_in)
+
+        tools: List[Dict[str, Any]] = dict_out.get("tools")
+        first_tool: Dict[str, Any] = tools[0]
+        extractor: DictionaryExtractor = DictionaryExtractor(first_tool)
+
+        value: Any = extractor.get("function.sly_data_schema")
+        self.assertIsNotNone(value)
+        self.assertIsInstance(value, dict)
+
+        value: Any = extractor.get("function.sly_data_schema.required")
+        self.assertIsNotNone(value)
+        self.assertIsInstance(value, list)
+        self.assertEqual(len(value), 1)
+
+    def test_sly_data_schema_both(self):
+        """
+        Tests when sly_data_schema is in both the global defs and local defs
+        """
+        my_filter: ConfigFilter = self.get_filter()
+
+        dict_in: Dict[str, Any] = {
+            # Typical BYOK
+            "sly_data_schema": {
+                "type": "object",
+                "properties": {
+                    "llm_config": {
+                        "type": "object",
+                        "properties": {
+                            "openai_api_key": {
+                                "type": "string",
+                                "description": "The user's OpenAI API key"
+                            }
+                        }
+                    }
+                },
+                "required": ["llm_config"]
+            },
+            "tools": [
+                {
+                    "name": "foo",
+                    "function": {
+                        "description": "blah blah",
+                        # Typical BYOK
+                        "sly_data_schema": {
+                            "type": "object",
+                            "properties": {
+                                "custom_value": {
+                                    "type": "string",
+                                    "description": "A custom string value"
+                                }
+                            },
+                            "required": ["custom_value"]
+                        }
+                    }
+                }
+            ]
+        }
+
+        dict_out: Dict[str, Any] = my_filter.filter_config(dict_in)
+
+        tools: List[Dict[str, Any]] = dict_out.get("tools")
+        first_tool: Dict[str, Any] = tools[0]
+        extractor: DictionaryExtractor = DictionaryExtractor(first_tool)
+
+        value: Any = extractor.get("function.sly_data_schema")
+        self.assertIsNotNone(value)
+        self.assertIsInstance(value, dict)
+
+        value: Any = extractor.get("function.sly_data_schema.properties")
+        self.assertIsNotNone(value)
+        self.assertIsInstance(value, dict)
+        self.assertEqual(len(value.keys()), 2)
+
+        value: Any = extractor.get("function.sly_data_schema.required")
+        self.assertIsNotNone(value)
+        self.assertIsInstance(value, list)
+        self.assertEqual(len(value), 2)
+
