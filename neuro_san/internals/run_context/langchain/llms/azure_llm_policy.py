07576385be002f849139fa7cd28243b0a3b7c736
Dan Fink <Daniel.Fink@cognizant.com>
2026-06-22 17:01:51 -0700

    Allow AZURE_OPENAI_ENDPOINT to come from llm_config

--- neuro_san/internals/run_context/langchain/llms/azure_llm_policy.py
+++ neuro_san/internals/run_context/langchain/llms/azure_llm_policy.py
@@ -173,8 +173,7 @@ class AzureLlmPolicy(OpenAILlmPolicy):
             verbose=False,
 
             # Azure-specific group that should be None if we have an client
-            azure_endpoint=self.get_value_or_env(config, "azure_endpoint",
-                                                 "AZURE_OPENAI_ENDPOINT", client),
+            azure_endpoint=self.get_value_or_env(config, "azure_endpoint", "AZURE_OPENAI_ENDPOINT"),
             deployment_name=self.get_value_or_env(config, "deployment_name",
                                                   "AZURE_OPENAI_DEPLOYMENT_NAME", client),
             openai_api_version=self.get_value_or_env(config, "openai_api_version", "OPENAI_API_VERSION"),
