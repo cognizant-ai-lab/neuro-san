ca81e3c9c6054722346f375a782b417bd8019d13
Dan Fink <Daniel.Fink@cognizant.com>
2026-07-11 11:32:09 -0700

    Add comments

--- neuro_san/service/watcher/temp_networks/s3_reservations_writer.py
+++ neuro_san/service/watcher/temp_networks/s3_reservations_writer.py
@@ -87,7 +87,10 @@ class S3ReservationsWriter:
         self.converter = ReservationDictionaryConverter()
 
         # Boto Machinations
+        # We should be able to have a single Session for the lifetime of this object
         self.session: AioSession = None
+
+        # Cached frozed credentials which can be invalidated should they expire
         self.frozen_credentials: ReadOnlyCredentials = None
 
     async def add_reservations(self, reservations_dict: Dict[Reservation, Any],
