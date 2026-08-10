554fbe8ac779a777c7faba4294253562d88b9ed6
Dan Fink <Daniel.Fink@cognizant.com>
2026-08-10 10:39:44 -0700

    Rediret JsonStructureParser

---
+++ neuro_san/internals/parsers/__init__.py
@@ -0,0 +1,25 @@
+from typing import Any
+from typing import Type
+
+from neuro_san.internals.utils.deprecation_redirect import DeprecationRedirect
+
+_DEPRECATION_REDIRECT = DeprecationRedirect(
+    __name__,
+    # A map from old class name to new class name for compatibility
+    {
+        "JsonStructureParser": {
+            "old_module": "structure.json_structure_parser",
+            "new_class": "neuro_san.message.parsers.structure.json_structure_parser.JsonStructureParser",
+            "warned": False,
+        },
+    }
+)
+
+
+def __getattr__(old_class: str) -> Type[Any]:
+    """
+    Redirect deprecated classes
+    :param old_class: The old class name
+    :return: The redirected class
+    """
+    return _DEPRECATION_REDIRECT.redirect_class(old_class)
