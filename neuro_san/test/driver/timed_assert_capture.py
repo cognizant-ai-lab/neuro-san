83c95da469265a37550bff9b0d358d9ea74df802
Andrei Denissov <andrei.denissov@cognizant.com>
2026-09-11 16:33:14 -0700

    Add test_index property.

--- neuro_san/test/driver/timed_assert_capture.py
+++ neuro_san/test/driver/timed_assert_capture.py
@@ -15,6 +15,8 @@
 #
 # END COPYRIGHT
 
+from typing import Optional
+
 from neuro_san.test.interfaces.assert_forwarder import AssertForwarder
 from neuro_san.test.driver.assert_capture import AssertCapture
 
@@ -25,14 +27,16 @@ class TimedAssertCapture(AssertCapture):
     whose asserts are being captured.
     """
 
-    def __init__(self, basis: AssertForwarder):
+    def __init__(self, basis: AssertForwarder, test_index: Optional[int] = None):
         """
         Constructor
 
         :param basis: AssertForwarder
+        :param test_index: Optional index of the test case for logging and reporting.
         """
         super().__init__(basis)
         self.execution_time_seconds: float = 0
+        self.test_index: Optional[int] = test_index
 
     def set_execution_time(self, execution_time_seconds: float):
         """
@@ -49,3 +53,11 @@ class TimedAssertCapture(AssertCapture):
         :return: Execution time in seconds
         """
         return self.execution_time_seconds
+
+    def get_test_index(self) -> Optional[int]:
+        """
+        Get the index of the test case for logging and reporting.
+
+        :return: Test index or None if not set
+        """
+        return self.test_index
